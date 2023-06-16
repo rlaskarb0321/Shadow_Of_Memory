c64181bc8c9b28dca158ec1be0be3fae890d7ab3
KkabyDol <rlaskarb0321@gmail.com>
2023-06-16 21:51:26 +0900

    컷신관련 기본적인것 완료, yarn spinner를 활용한 대화테스트도 끝
    * 컷신관련 기본적인것 완료
    1. 첫 컷신은 할 수 있다.
    2. 그 후로 컷신은 어케해야하는지 아직 모르겠음
    
    * yarn spinner
    1. 테스트로 동작은 어떻게 되는지 확인완료
    2. 그 후로 타임라인이랑 묶어서 어떻게 해야할것같은데 그건 잘 모르겠음

--- Assets/Resources/TestProduction/TestProductionMixerBehaviour.cs
+++ Assets/Resources/TestProduction/TestProductionMixerBehaviour.cs
@@ -8,16 +8,7 @@ public class TestProductionMixerBehaviour : PlayableBehaviour
     // NOTE: This function is called at runtime and edit time.  Keep that in mind when setting the values of properties.
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
-        //int inputCount = playable.GetInputCount();
-
-        //for (int i = 0; i < inputCount; i++)
-        //{
-        //    float inputWeight = playable.GetInputWeight(i);
-        //    ScriptPlayable<TestProductionBehaviour> inputPlayable = (ScriptPlayable<TestProductionBehaviour>)playable.GetInput(i);
-        //    TestProductionBehaviour input = inputPlayable.GetBehaviour ();
-        //}
-
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("입력입력");
         }
