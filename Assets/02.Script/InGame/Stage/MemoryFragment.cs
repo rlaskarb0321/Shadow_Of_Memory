fa5121a797b1f6cc995d49aef616df4fc7767648
KkabyDol <rlaskarb0321@gmail.com>
2023-06-20 10:57:39 +0900

    기억조각 애니메이션, 연출 세팅
    * 기억조각 애니메이션
    1. 빈 오브젝트 아래에 기억조각을 넣고 애니메이션 실행시켜서 각기 다른 위치에서 같은 애니메이션 수행
    
    * 연출 세팅
    1. 기억조각의 콜리전과 플레이어가 충돌하면 해당 조각이 연출을 시작시키도록 세팅함

--- Assets/02.Script/InGame/Stage/MemoryFragment.cs
+++ Assets/02.Script/InGame/Stage/MemoryFragment.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class MemoryFragment : MonoBehaviour
 {
+    //[SerializeField]
+    //private int _fragNumber;
     [SerializeField]
-    private int _fragNumber;
+    private PlayableDirector _fragProduction;
+
+    private void Update()
+    {
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             collision.GetComponent<PlayerCtrl>().GetMemoryFragment();
-            Destroy(gameObject);
+            // Destroy(gameObject);
+            ProductionMgr.StartProduction(_fragProduction);
         }
     }
 }
